1e59f7b124a6eee3f335bfa1feceea1bb743983d
SuryaPrakash2003 <ariessurya8124@gmail.com>
2023-02-10 09:21:32 +0530

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+// const ul = document.getElementsByClassName('circles')[0];
+// for(let i = 0; i < 25; i++){
+//     const li = document.createElement('li');
+//     const randomDelay = Math.floor(Math.random() * (25-10))+10;
+//     const randomWidth = Math.floor(Math.random() * (40-20))+20;
+//     console.log(li);
+//     li.style = ` position: absolute;
+//   display: block;
+//   left: ${randomWidth }%;
+//   list-style: none;
+//   width: ${randomWidth}px;
+//   height: ${randomWidth}px;
+//   background: rgba(255, 255, 255, 0.2);
+//   animation: animate linear infinite;
+//   animtaion-delay: ${randomDelay}s ;
+//   animation-duration: 30s;
+//   bottom: -150px;`
+//     ul.appendChild(li);
+// }
+// console.log(ul);
