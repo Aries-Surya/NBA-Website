398b1414392102404b24a4d2c588ac1cc88aa7f6
aries-surya <ariessurya8124@gmail.com>
2024-02-26 18:12:54 +0530

    Update 26/02/2024

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,53 @@
-// const ul = document.getElementsByClassName('circles')[0];
-// for(let i = 0; i < 25; i++){
-//     const li = document.createElement('li');
-//     const randomDelay = Math.floor(Math.random() * (25-10))+10;
-//     const randomWidth = Math.floor(Math.random() * (40-20))+20;
-//     console.log(li);
-//     li.style = ` position: absolute;
-//   display: block;
-//   left: ${randomWidth }%;
-//   list-style: none;
-//   width: ${randomWidth}px;
-//   height: ${randomWidth}px;
-//   background: rgba(255, 255, 255, 0.2);
-//   animation: animate linear infinite;
-//   animtaion-delay: ${randomDelay}s ;
-//   animation-duration: 30s;
-//   bottom: -150px;`
-//     ul.appendChild(li);
-// }
-// console.log(ul);
+
+const special = document.getElementsByClassName('special')[0];
+const hidden = document.getElementsByClassName('special-hidden')[0];
+special.addEventListener('click', () => {
+    hidden.classList.toggle('show');
+    special.classList.toggle('change-color')
+});
+
+
+
+const special1 = document.getElementsByClassName('special1')[0];
+const hidden1 = document.getElementsByClassName('special-hidden1')[0];
+special1.addEventListener('click', () => {
+    hidden1.classList.toggle('show1');
+    special1.classList.toggle('change-color1')
+});
+
+
+const special2 = document.getElementsByClassName('special2')[0];
+const hidden2 = document.getElementsByClassName('special-hidden2')[0];
+special2.addEventListener('click', () => {
+    hidden2.classList.toggle('show2');
+    special2.classList.toggle('change-color2')
+});
+
+
+const special3 = document.getElementsByClassName('special3')[0];
+const hidden3 = document.getElementsByClassName('special-hidden3')[0];
+special3.addEventListener('click', () => {
+    hidden3.classList.toggle('show3');
+    special3.classList.toggle('change-color3')
+});
+
+
+const special4 = document.getElementsByClassName('special4')[0];
+const hidden4 = document.getElementsByClassName('special-hidden4')[0];
+special4.addEventListener('click', () => {
+    hidden4.classList.toggle('show4');
+    special4.classList.toggle('change-color4')
+});
+const special5 = document.getElementsByClassName('special5')[0];
+const hidden5= document.getElementsByClassName('special-hidden5')[0];
+special5.addEventListener('click', () => {
+    hidden5.classList.toggle('show5');
+    special5.classList.toggle('change-color5')
+});
+
+const special6 = document.getElementsByClassName('special6')[0];
+const hidden6= document.getElementsByClassName('special-hidden6')[0];
+special6.addEventListener('click', () => {
+    hidden6.classList.toggle('show6');
+    special6.classList.toggle('change-color6')
+});
