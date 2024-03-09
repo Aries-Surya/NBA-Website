39664c377716af030d96955e19572e016feb113b
aries-surya <ariessurya8124@gmail.com>
2024-03-09 09:40:14 +0530

    BackUp

---
+++ script.js
@@ -0,0 +1,53 @@
+
+const special = document.getElementsByClassName('special')[0];
+const hidden = document.getElementsByClassName('special-hidden')[0];
+special.addEventListener('click', () => {
+    hidden.classList.toggle('show');
+    special.classList.toggle('change-color')
+});
+
+
+
+const special1 = document.getElementsByClassName('special1')[0];
+const hidden1 = document.getElementsByClassName('special-hidden1')[0];
+special1.addEventListener('click', () => {
+    hidden1.classList.toggle('show1');
+    special1.classList.toggle('change-color1')
+});
+
+
+const special2 = document.getElementsByClassName('special2')[0];
+const hidden2 = document.getElementsByClassName('special-hidden2')[0];
+special2.addEventListener('click', () => {
+    hidden2.classList.toggle('show2');
+    special2.classList.toggle('change-color2')
+});
+
+
+const special3 = document.getElementsByClassName('special3')[0];
+const hidden3 = document.getElementsByClassName('special-hidden3')[0];
+special3.addEventListener('click', () => {
+    hidden3.classList.toggle('show3');
+    special3.classList.toggle('change-color3')
+});
+
+
+const special4 = document.getElementsByClassName('special4')[0];
+const hidden4 = document.getElementsByClassName('special-hidden4')[0];
+special4.addEventListener('click', () => {
+    hidden4.classList.toggle('show4');
+    special4.classList.toggle('change-color4')
+});
+const special5 = document.getElementsByClassName('special5')[0];
+const hidden5= document.getElementsByClassName('special-hidden5')[0];
+special5.addEventListener('click', () => {
+    hidden5.classList.toggle('show5');
+    special5.classList.toggle('change-color5')
+});
+
+const special6 = document.getElementsByClassName('special6')[0];
+const hidden6= document.getElementsByClassName('special-hidden6')[0];
+special6.addEventListener('click', () => {
+    hidden6.classList.toggle('show6');
+    special6.classList.toggle('change-color6')
+});
